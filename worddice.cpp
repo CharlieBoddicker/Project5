5c15641f710ab9a635bd43c1be09733196ba3fc2
OwenYoungblood <oyoungbl@vols.utk.edu>
2024-12-01 17:55:55 -0500

    Update worddice.cpp

--- worddice.cpp
+++ worddice.cpp
@@ -34,23 +34,26 @@ int main(int argc, char **argv)
         int wordLen = word.size();
         int totalNodes = numDice + wordLen + 2;
         vector<vector<int>> graph(totalNodes);
-                vector<vector<int>> reverseGraph(totalNodes);
+        vector<vector<int>> reverseGraph(totalNodes);
         int source = 0;
         int sink = totalNodes - 1;
+
         //add edges from source to each die node
         for (int j = 0; j < numDice; j++)
         {
             graph[source].push_back(j + 1);
         }
+
         //for each letter in the word, create a unique letter node
-        //map each letter occurrence in the word to a unique node
+        //map each letter occurence in the word to a unique node
         unordered_map<char, vector<int>> letterNodeMap;
         for (int i = 0; i < wordLen; i++)
         {
             letterNodeMap[word[i]].push_back(numDice + i + 1);
         }
+
         //add edges from dice nodes to the letter nodes if the dice contain the letter
-        unordered_map<int, unordered_set<int>> connectedNodes;//unordered set so that there are no duplicates
+        unordered_map<int, unordered_set<int>> connectedNodes; //unordered set so that there are no duplicates
         for (int i = 0; i < numDice; i++)
         {
             for (int j = 0; j < dice[i].size(); j++)
@@ -65,109 +68,109 @@ int main(int argc, char **argv)
                         if (connectedNodes[i + 1].find(letterNode) == connectedNodes[i + 1].end())
                         {
                             graph[i + 1].push_back(letterNode);
-                                                        reverseGraph[letterNode].push_back(i+1);
+                            reverseGraph[letterNode].push_back(i + 1);
                             connectedNodes[i + 1].insert(letterNode);
                         }
                     }
                 }
             }
         }
-
-
-                vector<int> nodePointers(totalNodes, 0); //number of edges pointing to each node
-                int numSink = 0; //number of edges pointing to the sink
-
-                for(int i = 0; i < graph.size(); i++)
-                {
-                        for(int j : graph[i]){
-                                nodePointers[j] = nodePointers[j] + 1;
-
-                        }
-                        //cout << nodePointers[i] << " ";
-
-
-                }
-                for (int i = 0; i < graph.size(); i++)
+        vector<int> nodePointers(totalNodes, 0);
+        for (int i = 0; i < graph.size(); i++)
         {
-            cout << "Node " << i << ": ";
-            if (i == source)
-            {
-                cout << "SOURCE ";
-            }
-            else if (i == sink)
-            {
-                cout << "SINK ";
-            }
-            else if (i <= numDice)
-            {
-                cout << dice[i - 1] << " ";
-            }
-            else
-            {
-                cout << word[i - numDice - 1] << " ";
-            }
-            cout << "Edges to";
             for (int j : graph[i])
             {
-                cout << " " << j;
+                nodePointers[j]++;
             }
-            cout << endl;
-            cout << nodePointers[i];
-            cout << endl;
-            cout << "Reverse Edges to";
-            for (int j : reverseGraph[i])
-            {
-                cout << " " << j;
-            }
-            cout << endl;
         }
-        cout << endl;
 
+        for (int i = 0; i < wordLen; i++)
+        {
+            int OGnode = -1;
+            int node = -1;
+            int letterNode = numDice + i + 1;
 
-                for(int i = 0; i < wordLen; i++){
-                        int OGnode = -1;
-                        int node = -1;
-                        int letterNode = numDice + i + 1;
-                        if(nodePointers[letterNode] == 1){
-                                OGnode = reverseGraph[letterNode][0];
-                                node = letterNode;
-                                cout << letterNode << "P: " << nodePointers[letterNode] << endl;
+            if (nodePointers[letterNode] == 1)
+            {
+                OGnode = reverseGraph[letterNode][0];
+                node = letterNode;
+                //cout << letterNode << "P: " << nodePointers[letterNode] << endl;
+            }
 
-                        }
-                        if(node != -1){
-                                //cout << letterNode << "P: " << nodePointers[letterNode] << endl;
-                                        for(int k = 0; k < graph[OGnode].size(); k++){
-                                                int j = graph[OGnode][k];
-                                                cout << "(j): " << j << endl;
-                                                cout << "Node" << node << endl;
-                                                cout << "OGnodeS" << graph[OGnode].size() << endl;
-                                                if(j != node && graph[OGnode].size() != 1){
-                                                        cout << j << endl;
-                                                        auto it = remove(graph[OGnode].begin(), graph[OGnode].end(), j);
-                                                        graph[OGnode].erase(it, graph[OGnode].end());
-                                                        nodePointers[j] = nodePointers[j] - 1;
-                                                        k--;
-                                                }
-                                        }
-                        }
+            if (node != -1)
+            {
+                for (int k = 0; k < graph[OGnode].size(); k++)
+                {
+                    int j = graph[OGnode][k];
+                    //cout << "(j): " << j << endl;
+                    //cout << "Node" << node << endl;
+                    //cout << "OGnodeS" << graph[OGnode].size() << endl;
+                    if (j != node && graph[OGnode].size() != 1)
+                    {
+                        //cout << j << endl;
+                        auto it = remove(graph[OGnode].begin(), graph[OGnode].end(), j);
+                        graph[OGnode].erase(it, graph[OGnode].end());
+                        nodePointers[j]--;
+                        k--;
+                    }
                 }
-                //add edges from each letter node to the sink
+            }
+        }
+
+        //add edges from each letter node to the sink
         for (int i = 0; i < wordLen; i++)
         {
             int letterNode = numDice + i + 1;
-
-            if(nodePointers[letterNode] > 0){
+            if (nodePointers[letterNode] > 0)
+            {
                 graph[letterNode].push_back(sink);
-                                //fix numsink calculations
-                                numSink++;
             }
         }
-                if(numSink != wordLen) cout << "Cannot spell " << word << endl;
-                else{
 
+        //check if the word can be spelled
+        int numSink = 0;
+        for (int i = 0; i < wordLen; i++)
+        {
+            int letterNode = numDice + i + 1;
+            if (nodePointers[letterNode] > 0)
+            {
+                numSink++;
+            }
+        }
+
+        if (numSink != wordLen)
+        {
+            cout << "Cannot spell " << word << endl;
+        }
+        else
+        {
+            //output the dice used in order to spell the word
+            vector<int> diceUsed;
+            for (int i = 0; i < wordLen; i++)
+            {
+                int letterNode = numDice + i + 1;
+                for (int j = 0; j < numDice; j++)
+                {
+                    if (find(graph[j + 1].begin(), graph[j + 1].end(), letterNode) != graph[j + 1].end())
+                    {
+                        diceUsed.push_back(j);
+                        break;
+                    }
                 }
+            }
 
-        for (int i = 0; i < graph.size(); i++)
+            //output the dice used in order
+            for (int i = 0; i < diceUsed.size(); i++)
+            {
+                cout << diceUsed[i];
+                if (i < diceUsed.size() - 1)
+                {
+                    cout << ",";
+                }
+            }
+            cout << ": " << word << endl;
+        }
+        /*for (int i = 0; i < graph.size(); i++)
         {
             cout << "Node " << i << ": ";
             if (i == source)
@@ -191,17 +194,9 @@ int main(int argc, char **argv)
             {
                 cout << " " << j;
             }
-                        cout << endl;
-                        cout << nodePointers[i];
-            cout << endl;
-                        cout << "Reverse Edges to";
-            for (int j : reverseGraph[i])
-            {
-                cout << " " << j;
-            }
             cout << endl;
         }
-        cout << endl;
+        cout << endl;*/
     }
 
     return 0;
